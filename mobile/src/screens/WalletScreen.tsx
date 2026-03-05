973e79bee8093379649d6ca764758a55f9f3a3a5
arturvojceh-ops <arturvojceh@gmail.com>
2026-03-05 19:58:02 +0300

    Create WalletScreen.tsx file

---
+++ mobile/src/screens/WalletScreen.tsx
@@ -0,0 +1,13 @@
+// WalletScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WalletScreen = () => {
+    return (
+        <View>
+            <Text>Wallet Screen</Text>
+        </View>
+    );
+};
+
+export default WalletScreen;
