a4f7f48f0911b1d6d6f8546552ee0ac8e8e8ccc2
arturvojceh-ops <arturvojceh@gmail.com>
2026-03-05 19:58:48 +0300

    Create SmartWallet.sol file

---
+++ contracts/SmartWallet.sol
@@ -0,0 +1,8 @@
+// SmartWallet.sol content fetched from arturvojceh-ops/enterprise-aa-platform
+// Add your SmartWallet.sol contract code here.
+
+pragma solidity ^0.8.0; // Example of Solidity version
+
+contract SmartWallet {
+    // Your contract code goes here...
+}
