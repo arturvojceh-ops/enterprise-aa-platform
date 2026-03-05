e450e9807774f5837744530acf3b037ec2249786
arturvojceh-ops <arturvojceh@gmail.com>
2026-03-05 19:57:40 +0300

    Create contracts/EnterprisePaymaster.sol using content from the original file.

---
+++ contracts/EnterprisePaymaster.sol
@@ -0,0 +1,7 @@
+// Content from EnterprisePaymaster.sol of arturvojceh-ops/enterprise-aa-platform repository
+
+pragma solidity ^0.8.0;
+
+contract EnterprisePaymaster {
+    // Your contract code goes here...
+}
